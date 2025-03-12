06397b03c20cc63aa46a61ed62bc9fcc96a47d6a
typpsii <kaedynhindley@gmail.com>
2025-03-12 08:26:42 -0700

    Coded CoinCounterUI

---
+++ Assets/_Scripts/CoinCounterUI.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using DG.Tweening;
+using TMPro;
+using UnityEngine;
+
+public class CoinCounterUI : MonoBehaviour
+{
+    [SerializeField] private TextMeshProGUI current;
+    [SerializeField] private TextMeshProGUI toUpdate;
+    [SerializeField] private Transform coinTextContainer;
+    [SerializeField] private float duration;
+    [SerializeField] private Ease animationCurve;
+
+
+    private float containerInitPosition;
+    private float moveAmount;
+
+    private void Start()
+    {
+        Canvas.ForceUpdateCanvases();
+        current.SetText("0");
+        toUpdate.SetText("0");
+        containerInitPosition = coinTextContainer.localPosition.y;
+        moveAmount = current.rectTransform.rect.height;
+    }
+
+    public void UpdateScore(int score)
+    {
+        toUpdate.SetText("${score}");
+        coinTextContainer.DOLocalMoveY(containerInitPosition + moveAmount, duration).SetEase(animationCurve);
+        StartCoroutine(ResetCoinContainer(score));
+    }
+
+    private IEnumerator ResetCoinContainer(int score)
+    {
+        yield return new WaitForSeconds(duration);
+        current.SetText("${score}");
+        Vector3 localPosition = coinTextContainer.localPosition;
+        coinTextContainer.localPosition = new Vector3(localPosition.x, containerInitPosition, localPosition.z);
+
+    }
+}
